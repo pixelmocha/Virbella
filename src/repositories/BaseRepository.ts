ce5ad752ca990c66bd9b8f67aee6fff4bc0f9233
pixelmocha <pixelmochasoftware@gmail.com>
2021-08-18 11:06:33 -0700

    Update BaseRepository.ts

--- src/repositories/BaseRepository.ts
+++ src/repositories/BaseRepository.ts
@@ -3,7 +3,9 @@
 // functionality. I was going to include the static repo instances
 // and introduce a generic to pass in the repo type to spin it up, but
 // the workaround for a generic on a static field was a bit hacky, and
-// I didn't have much time left to implement it.
+// I didn't have much time left to implement it. Also, I could have gotten
+// all the common methods like get-by-id to live here using a generic
+// to inject the data type.
 export abstract class BaseRepository {
     protected baseUrl: string = 'http://localhost:9001';
 }
